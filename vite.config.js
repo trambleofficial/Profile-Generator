a89cc956a0f54fe8584d7c7cb83f5e036e45957d
trambleofficial <trambleofficial@gmail.com>
2026-04-10 15:57:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: '0.0.0.0', // 🔥 allow all network connections
     port: 5173,
+    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
